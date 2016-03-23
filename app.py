60654d7f9f23fcdf53d19771124d2d736979ce88
Bryan Leung <fiftyone@gmail.com>
2016-03-23 14:35:45 -0700

    fixed indentation

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from flask_slack import Slack
 @post('/schedule')
 
 def schedule():
-	scheduleURL = 'https://docs.google.com/spreadsheets/d/1GD__Yyx8GZ6vsqo7E4pxqoitooOr5LgLomVAL8gBFH8/edit'
-	return scheduleURL
+    scheduleURL = 'https://docs.google.com/spreadsheets/d/1GD__Yyx8GZ6vsqo7E4pxqoitooOr5LgLomVAL8gBFH8/edit'
+    return scheduleURL
 
 if __name__ == '__main__':
-	port = os.environ.get('PORT', 5000)
-	run(host='0.0.0.0', port=port)
+    port = os.environ.get('PORT', 5000)
+    run(host='0.0.0.0', port=port)

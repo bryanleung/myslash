64e94b61510ff07ddf9f1b84683fb95268dd276a
Bryan Leung <fiftyone@gmail.com>
2016-03-23 14:33:29 -0700

    reverted the new stuff

--- app.py
+++ app.py
@@ -5,20 +5,11 @@ import os
 from bottle import run, post
 from flask_slack import Slack
 
-slack = Slack(app)
-app.add_url_rule('/', view_func=slack.dispatch)
-
 @post('/schedule')
-
-@slack.command('your_command', token='hMj5SZXTCJJUqO4FnUoE1Jgd', team_id='T0001', methods=['POST'])
-def your_method(**kwargs):
-    text = kwargs.get('text')
-    return slack.response(text)
-
 
 def schedule():
 	scheduleURL = 'https://docs.google.com/spreadsheets/d/1GD__Yyx8GZ6vsqo7E4pxqoitooOr5LgLomVAL8gBFH8/edit'
-	return slack.response(scheduleURL)
+	return scheduleURL
 
 if __name__ == '__main__':
 	port = os.environ.get('PORT', 5000)

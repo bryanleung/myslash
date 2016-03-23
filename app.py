3d7481797a4fcf5c5c770df3af9350b687bcc54e
Bryan Leung <fiftyone@gmail.com>
2016-03-23 14:04:55 -0700

    test

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from bottle import run, post
 @post('/schedule')
 def schedule():
 	scheduleURL = 'https://docs.google.com/spreadsheets/d/1GD__Yyx8GZ6vsqo7E4pxqoitooOr5LgLomVAL8gBFH8/edit'
+	print("func() in one.py")
 	return scheduleURL
 
 if __name__ == '__main__':
